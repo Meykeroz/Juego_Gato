c15fbae30506f73871788f5b27415ee515079739
Manuel Alvarez <manuel.alvarez@lpcoders.com>
2022-05-27 16:00:20 -0700

    Ultimos cambios

--- script.js
+++ script.js
@@ -1,129 +1,139 @@
     const tablero = ["","","","","","","","",""];
     var turn ="X";
     var plays = 0;
+    var winner = false;
     
     document.getElementById("player").innerHTML = turn;
 
     function game(cell){
-        if (tablero[cell] == 'X'){
-            console.log("No puede ingresar");
-        }else if(tablero[cell] == 'O'){
-            console.log("No puede ingresar");
-        }else{
-            if (turn == "X") {
-                tablero[cell]=document.getElementById(cell).innerHTML = "X";
-                turn = "O";
-                document.getElementById("player").innerHTML = turn;
-                
+        
+        if (winner == false) {
+            if (tablero[cell] == 'X'){
+                console.log("No puede ingresar");
+            }else if(tablero[cell] == 'O'){
+                console.log("No puede ingresar");
             }else{
-                tablero[cell]=document.getElementById(cell).innerHTML = "O";
-                turn = "X";
-                document.getElementById("player").innerHTML = turn;
-                
-            }
-            validation();
-        }
+                if (turn == "X") {
+                    tablero[cell]=document.getElementById(cell).innerHTML = "X";
+                    turn = "O";
+                    document.getElementById("player").innerHTML = turn;
+                    validation();
+                    plays++;
+                }else{
+                    tablero[cell]=document.getElementById(cell).innerHTML = "O";
+                    turn = "X";
+                    document.getElementById("player").innerHTML = turn;
+                    validation();
+                    plays++;
+                }     
+            } 
+         }else{
+             console.log("Juego terminado!");
+         }
     }
 
     function validation(){
-        plays++;
-        var winner=document.getElementById("winner");
 
-        if (plays < 9)
-         {
             // Validacion de filas
 
             if("X" == tablero[0] && "X" == tablero[1] && "X" == tablero[2]){
-                alert("Gano la X");
+                //alert("Gano la X");
                 //winner.innerHTML = "X";
-                window.location.reload();
+                document.getElementById("label").innerHTML = "El ganador es :";
+                document.getElementById("player").innerHTML = "X";
+                winner=true;
                 
             } else if("O" == tablero[0] && "O" == tablero[1] && "O" == tablero[2]){
                 //winner.innerHTML = "O";
-                alert("Gano la O");
-                window.location.reload();
+                document.getElementById("label").innerHTML = "El ganador es :";
+                document.getElementById("player").innerHTML = "O";
+                winner=true;
             }
 
             if("X" == tablero[3] && "X" == tablero[4] && "X" == tablero[5]){
-                alert("Gano la X");
-                //winner.innerHTML = "X";
-                window.location.reload();
+                document.getElementById("label").innerHTML = "El ganador es :";
+                document.getElementById("player").innerHTML = "X";
+                winner=true;
             } else if("O" == tablero[3] && "O" == tablero[4] && "O" == tablero[5]){
-                //winner.innerHTML = "O";
-                alert("Gano la O");
-                window.location.reload();
+                document.getElementById("label").innerHTML = "El ganador es :";
+                document.getElementById("player").innerHTML = "O";
+                winner=true;
             }
 
             if("X" == tablero[6] && "X" == tablero[7] && "X" == tablero[8]){
-                 //winner.innerHTML = "X";
-                 alert("Gano la X");
-                window.location.reload();
+                document.getElementById("label").innerHTML = "El ganador es :";
+                document.getElementById("player").innerHTML = "X";
+                winner=true;
             } else if("O" == tablero[6] && "O" == tablero[7] && "O" == tablero[8]){
-                //winner.innerHTML = "O";
-                alert("Gano la O");
-                window.location.reload();
+                document.getElementById("label").innerHTML = "El ganador es :";
+                document.getElementById("player").innerHTML = "O";
+                winner=true;
             }
 
             //Validacion de columnas
 
             if("X" == tablero[0] && "X" == tablero[3] && "X" == tablero[6]){
-                //winner.innerHTML = "X";
-                alert("Gano la X");
-                window.location.reload();
+                document.getElementById("label").innerHTML = "El ganador es :";
+                document.getElementById("player").innerHTML = "X";
+                winner=true;
             } else if("O" == tablero[0] && "O" == tablero[3] && "O" == tablero[6]){
-                //winner.innerHTML = "O";
-                alert("Gano la O");
-                window.location.reload();
+                document.getElementById("label").innerHTML = "El ganador es :";
+                document.getElementById("player").innerHTML = "O";
+                winner=true;
             }
 
             if("X" == tablero[1] && "X" == tablero[4] && "X" == tablero[7]){
-                //winner.innerHTML = "X";
-                alert("Gano la X");
-                window.location.reload();
+                document.getElementById("label").innerHTML = "El ganador es :";
+                document.getElementById("player").innerHTML = "X";
+                winner=true;
             } else if("O" == tablero[1] && "O" == tablero[4] && "O" == tablero[7]){
-                //winner.innerHTML = "O";
-                alert("Gano la O");
-                window.location.reload();
+                document.getElementById("label").innerHTML = "El ganador es :";
+                document.getElementById("player").innerHTML = "O";
+                winner=true;
             }
 
             if("X" == tablero[2] && "X" == tablero[5] && "X" == tablero[8]){
-                //winner.innerHTML = "X";
-                alert("Gano la X");
-                window.location.reload();
+                document.getElementById("label").innerHTML = "El ganador es :";
+                document.getElementById("player").innerHTML = "X";
+                winner=true;
             } else if("O" == tablero[2] && "O" == tablero[5] && "O" == tablero[8]){
-                //winner.innerHTML = "O";
-                alert("Gano la O");
-                window.location.reload();
+                document.getElementById("label").innerHTML = "El ganador es :";
+                document.getElementById("player").innerHTML = "O";
+                winner=true;
             }
 
             //Validacion de diagonales
 
             if("X" == tablero[0] && "X" == tablero[4] && "X" == tablero[8]){
-                //winner.innerHTML = "X";
-                alert("Gano la X");
-                window.location.reload();
+                document.getElementById("label").innerHTML = "El ganador es :";
+                document.getElementById("player").innerHTML = "X";
+                winner=true;
             } else if("O" == tablero[0] && "O" == tablero[4] && "O" == tablero[8]){
-                //winner.innerHTML = "O";
-                alert("Gano la O");
-                window.location.reload();
+                document.getElementById("label").innerHTML = "El ganador es :";
+                document.getElementById("player").innerHTML = "O";
+                winner=true;
             }
 
             if("X" == tablero[2] && "X" == tablero[4] && "X" == tablero[6]){
-                //winner.innerHTML = "X";
-                alert("Gano la X");
-                window.location.reload();
+                document.getElementById("label").innerHTML = "El ganador es :";
+                document.getElementById("player").innerHTML = "X";
+                winner=true;
             } else if("O" == tablero[2] && "O" == tablero[4] && "O" == tablero[6]){
-                //winner.innerHTML = "O";
-                alert("Gano la O");
-                window.location.reload();
+                document.getElementById("label").innerHTML = "El ganador es :";
+                document.getElementById("player").innerHTML = "O";
+                winner=true;
             }
-        }else{
-                 alert("Empate!");
-                 window.location.reload();
-                 //winner.innerHTML = "Empate!";
-             }
-        
+        if(plays == 9){
+            document.getElementById("label").style.background = "yellow";
+            document.getElementById("label").innerHTML = "Empate!";
+            document.getElementById("player").innerHTML = " ";
+        }
+     }   
+    
+    function reload(){
+        window.location.reload();
     }
 
 
                 
+    
